145f5890dab5b64b8bbe81d3a86ddcecee41c55f
KONRADSPC\khafe <konrad.hafen@gmail.com>
2019-09-12 09:29:57 -0700

    first commit of package

---
+++ README.rst
@@ -0,0 +1,8 @@
+hydat
+=====
+
+hydat is a python package for downloading various hydrologic data. I developed this package for personal use and not for widespread
+distribution.
+
+Depedencies
+-----------
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='hydat',
+      version='0.0.9',
+      description='Download hydrologic data',
+      long_description=readme(),
+      classifiers=[
+            'Development Status :: 3 - Alpha',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Natural Language :: English',
+            'Topic :: Scientific/Engineering :: GIS',
+      ],
+      keywords='hydrology, data, download, climate',
+      url='https://github.com/konradhafen/hydat',
+      author='Konrad Hafen',
+      author_email='khafen74@gmail.com',
+      license='GPLv3',
+      packages=['hydat'],
+      install_requires=[
+
+      ],
+      include_package_data=True,
+      zip_safe=False)
