00c7efa993f608127ed72c83b6ace02a2ddba95b
KONRADSPC\khafe <konrad.hafen@gmail.com>
2020-02-14 12:07:33 -0800

    function to combine monthly grids to 2d array and save

--- hydat/utils.py
+++ hydat/utils.py
@@ -1,4 +1,26 @@
 import numpy as np
+import gdal
 
-def monthlyGridsToNumpy():
-    pass
+
+def getPRISMFilename(year, month, var, res='4km'):
+    fnbase = 'PRISM'
+    mversion = 'M3'
+    if year < 1981:
+        if var == 'ppt':
+            mversion = 'M2'
+    fn = fnbase + '_' + var + '_stable_' + res + mversion + '_' + str(year) + str(month).zfill(2) + '_bil.bil'
+    return fn
+
+
+def monthlyGridsToNumpy(out_fn, start_year, end_year, in_dir, pvar):
+    ncol = (end_year - start_year + 1) * 12
+    out_arr = None
+    for year in range(start_year, end_year + 1):
+        for month in range(0, 12):
+            ds = gdal.Open(in_dir + '/' + getPRISMFilename(year, month + 1, pvar))
+            temp_arr = ds.GetRasterBand(1).ReadAsArray()
+            if out_arr is None:
+                out_arr = np.empty((temp_arr.size, ncol), dtype=np.float)
+            colidx = (year - start_year) * 12 + month
+            out_arr[:, colidx] = temp_arr.flatten()
+    np.save(out_fn)
